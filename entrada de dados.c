34755ed1a6f9eda6bce578ddaa87f4f0a06f3f3f
Henrique Gibi <henriquedepadua@yahoo.com.br>
2021-05-20 11:25:41 -0300

    Criando funcao de entrada de dados

--- entrada de dados.c
+++ entrada de dados.c
@@ -1,40 +1,48 @@
 #include <stdio.h>
+#include <string.h>
 
-void limpar_entrada() {
+void limpar_entrada()
+{
     char c;
     while ((c = getchar()) != '\n' && c != EOF) {}
 }
 
-int main () {
+void ler_texto(char *buffer, int length)
+{
+    fgets(buffer, length, stdin);
+    strtok(buffer, "\n");
+}
 
-    int idade1, idade2;
-
+int main()
+{
+    double salario1, salario2;
     char nome1[50], nome2[50];
+    int idade;
+    char sexo;
 
-    printf("Digite o valor da idade 1: ");
-    scanf("%d", &idade1);
-    printf("Digite o nome da pessoa 1: ");
+    printf("Nome da primeira pessoa: ");
+    ler_texto(nome1, 50);
+    printf("Salario da primeira pessoa: ");
+    scanf("%lf", &salario1);
+
+    printf("Nome da segunda pessoa: ");
     limpar_entrada();
-    fgets(nome1, 50, stdin);
-    strtok(nome1, "\n");
+    ler_texto(nome2, 50);
+    printf("Salario da segunda pessoa: ");
+    scanf("%lf", &salario2);
 
-    printf("\n");
-
-    printf("Digite o valor da idade 2: ");
-    scanf("%d", &idade2);
-    printf("Digite o nome da pessoa 2: ");
+    printf("Digite uma idade: ");
+    scanf("%d", &idade);
+    printf("Digite um sexo (F/M): ");
     limpar_entrada();
-    fgets(nome2, 50, stdin);
-    strtok(nome1, "\n");
+    scanf("%c", &sexo);
 
-    printf("\n");
-
-    printf("Idade 1 = %d\n", idade1);
-    printf("Nome 1 = %s\n", nome1);
-    printf("\n");
-    printf("Idade 2 = %d\n", idade2);
-    printf("Nome 2 = %s\n", nome2);
-    printf("\n");
+    printf("Nome 1: %s\n", nome1);
+    printf("Salario 1: %.2lf\n", salario1);
+    printf("Nome 2: %s\n", nome2);
+    printf("Salario 2: %.2lf\n", salario2);
+    printf("Idade: %d\n", idade);
+    printf("Sexo: %c\n", sexo);
 
     return 0;
 }
